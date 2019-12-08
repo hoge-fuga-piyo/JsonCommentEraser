e52c37296fbbf0886bb72a269abd7be9457d0e42
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2019-12-08 22:01:16 +0900

    空行を削除する機能を実装

--- jsonCommentEraser.py
+++ jsonCommentEraser.py
@@ -8,6 +8,11 @@ comment_pattern = r"(\s|\t)*//"
 with open(args[1], 'r', newline='') as f:
   for line in f:
     is_comment = False
+    # 空行を削除:w
+    if line.strip() == '':
+      continue
+
+    # コメント行を削除
     matchOB = re.match(comment_pattern, line)
     if matchOB:
       is_comment = True
